3622d379946761e2c3c6ff0977d3dea4f15c8ae9
Nero <darkultrapsp.mc@gmail.com>
2020-03-26 20:07:45 +0100

    Ajout de taches commencement header

--- Developpement/include/header.php
+++ Developpement/include/header.php
@@ -1,3 +1,10 @@
 <header>
-    
+    <div class="bandeau">
+        <div class="image">
+        </div>
+        <div class="nomResto">
+        </div>
+        <div class="connexion">
+        </div>
+    </div>
 </header>
