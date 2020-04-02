130c37ab9b3834f236b2b5a4dc3ca1e1444be7a1
Mathilde <mathildebourinet@orange.fr>
2020-04-02 15:41:32 +0200

    maj

--- Developpement/Connexion/Connexion.php
+++ Developpement/Connexion/Connexion.php
@@ -1,2 +1,24 @@
 <?php
 
+    include ("include/header.php");
+    
+?>
+<link rel="stylesheet" type="text/css" href="css/connexion.css" media="all"/>
+<div class="bloc">
+    <p class="titre">Connexion</p>
+    <div class="interieur">
+        <form method="POST" action="SessionManager.php">
+            <p class="titre2">Pseudo :</p>
+            <input class="barre" type ="text" name="pseudo" placeholder="Votre pseudo" required/>
+            <p class="titre2"> Mot de passe :</p>
+            <input class="barre" type ="password" name="password" placeholder="Votre mot de passe" required/>
+            </br>
+            </br>  
+            <input class="barre" type="submit" value="Connexion"/>
+        </form> 
+    </div>
+</div>
+
+<?php
+    include("include/footer.php")
+?>
--- Developpement/include/header.php
+++ Developpement/include/header.php
@@ -2,7 +2,6 @@
 include_once 'dataManager/databaseLinker.php';
 include_once 'dataManager/ClientManager.php';
 include_once 'dataManager/CommandeManager.php';
-
 include_once 'data/Client.php';
 include_once 'data/Commande.php';
 include_once 'data/Produit.php';
@@ -14,7 +13,7 @@ include_once 'data/TypeProduit.php';
 <html lang="fr">
     <head>
         <meta charset="utf-8" />
-        <title>Forum PPE01</title>
+        <title>Tacos of all time</title>
         <link rel="stylesheet" type="text/css" href="css/header.css" media="all"/>
         <link rel="stylesheet" type="text/css" href="css/Organisation.css" media="all"/>
 
@@ -24,6 +23,7 @@ include_once 'data/TypeProduit.php';
 <header>
     <div class="bandeau">
         <div class="image">
+            <img src="image/banniere2.jpg"/>
         </div>
         <div class="nomResto">
         </div>
