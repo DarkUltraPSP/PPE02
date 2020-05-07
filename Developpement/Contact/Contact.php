12e6b4d10eb372b2406ea8b5e4d84b54df280f20
Nero <darkultrapsp.mc@gmail.com>
2020-05-07 10:43:13 +0200

    contact

--- Developpement/Contact/Contact.php
+++ Developpement/Contact/Contact.php
@@ -1,9 +1,46 @@
-<div class="descriptions">
-    <p> Qui sommes-nous ? </p>
-    <p> Tacos Of All Time est une entreprise nouvelle avec uniquement des employé qualifié. De plus, notre Maitre Kebabiste est un chef étoilé de renommée internationale. </p>
-</div>
-<div class="horaires">
-    <p> Nos Horaires </p>
-    <p> Du Mardi au Samedi 11h30 - 15h 18h30 - 23h </p>
-    <p> Du Dimanche au Lundi 11h30 - 15h </p>
+<div class="containerContact">
+    
+    <div class="descriptions">
+        
+        <p> Qui sommes-nous ? </p>
+        <p> Tacos Of All Time est une entreprise nouvelle avec uniquement des employé qualifié. De plus, notre Maitre Kebabiste est un chef étoilé de renommée internationale. </p>
+        
+    </div>
+    
+    <div class="horaires">
+        
+        <p> Nos Horaires :</p>
+        <table>
+            <tr>
+                <td> Lundi </td>
+                <td> Ferme </td>
+            </tr>
+            <tr>
+                <td> Mardi </td>
+                <td> 11h30 - 15h 18h30 - 22h </td>
+            </tr>
+            <tr>
+                <td> Mercredi </td>
+                <td> 11h30 - 15h 18h30 - 23h</td>
+            </tr>
+            <tr>
+                <td> Jeudi </td>
+                <td> 11h30 - 15h 18h30 - 23h</td>
+            </tr>
+            <tr>
+                <td> Vendredi </td>
+                <td> 11h30 - 15h 18h30 - 23h</td>
+            </tr>
+            <tr>
+                <td> Samedi </td>
+                <td> 11h30 - 15h 18h30 - 23h</td>
+            </tr>
+            <tr>
+                <td> Dimanche </td>
+                <td> 18h30 - 21h30 </td>
+            </tr>
+        </table>
+        
+    </div>
+    
 </div>
