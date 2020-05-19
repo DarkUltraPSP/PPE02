205ada0f966a6e6ff4d34d70a7f653ff1edec7c8
Mathilde <mathildebourinet@orange.fr>
2020-05-20 00:53:07 +0200

    contact css pas fini

--- Developpement/Pages/Contact/Contact.php
+++ Developpement/Pages/Contact/Contact.php
@@ -1,54 +1,61 @@
-<div class="containerContact">
-    
-    <div class="descriptions">
-        
-        <p> Qui sommes-nous ? </p>
-        <p> Tacos Of All Time est une entreprise nouvelle avec uniquement des employé qualifié. De plus, notre Maitre Kebabiste est un chef étoilé de renommée internationale. </p>
-        
+<!DOCTYPE html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="css/contact.css" media="all"/>
+    </head>
+    <body>
+    <div class="containerContact">
+
+        <div class="descriptions">
+
+            <p class="entete"> Qui sommes-nous ? </p>
+            <p class="texte"> Tacos Of All Time est une entreprise nouvelle avec uniquement des employés qualifiés. De plus, notre Maitre Kebabiste est un chef étoilé de renommée internationale. </p>
+
+        </div>
+
+        <div class="horaires">
+
+            <p class="entete"> Nos Horaires :</p>
+            <table>
+                <tr>
+                    <td class="jour"> Lundi </td>
+                    <td> Ferme </td>
+                </tr>
+                <tr>
+                    <td class="jour"> Mardi </td>
+                    <td> 11h30 - 15h 18h30 - 22h </td>
+                </tr>
+                <tr>
+                    <td class="jour"> Mercredi </td>
+                    <td> 11h30 - 15h 18h30 - 23h</td>
+                </tr>
+                <tr>
+                    <td class="jour"> Jeudi </td>
+                    <td> 11h30 - 15h 18h30 - 23h</td>
+                </tr>
+                <tr>
+                    <td class="jour"> Vendredi </td>
+                    <td> 11h30 - 15h 18h30 - 23h</td>
+                </tr>
+                <tr>
+                    <td class="jour"> Samedi </td>
+                    <td> 11h30 - 15h 18h30 - 23h</td>
+                </tr>
+                <tr>
+                    <td class="jour"> Dimanche </td>
+                    <td> 18h30 - 21h30 </td>
+                </tr>
+            </table>
+
+        </div>
+
+        <div>
+            <p> Nous contacter par téléphone : 04 98 65 45 89 </p>
+        </div>
+
+        <div>
+            <p> A partir du 22 mai nous commencerons a prendre les commandes en ligne.</p>
+        </div>
+
     </div>
-    
-    <div class="horaires">
-        
-        <p> Nos Horaires :</p>
-        <table>
-            <tr>
-                <td> Lundi </td>
-                <td> Ferme </td>
-            </tr>
-            <tr>
-                <td> Mardi </td>
-                <td> 11h30 - 15h 18h30 - 22h </td>
-            </tr>
-            <tr>
-                <td> Mercredi </td>
-                <td> 11h30 - 15h 18h30 - 23h</td>
-            </tr>
-            <tr>
-                <td> Jeudi </td>
-                <td> 11h30 - 15h 18h30 - 23h</td>
-            </tr>
-            <tr>
-                <td> Vendredi </td>
-                <td> 11h30 - 15h 18h30 - 23h</td>
-            </tr>
-            <tr>
-                <td> Samedi </td>
-                <td> 11h30 - 15h 18h30 - 23h</td>
-            </tr>
-            <tr>
-                <td> Dimanche </td>
-                <td> 18h30 - 21h30 </td>
-            </tr>
-        </table>
-        
-    </div>
-    
-    <div>
-        <p> Nous contacter par téléphone : 04 98 65 45 89 </p>
-    </div>
-    
-    <div>
-        <p> A partir du 22 mai nous commencerons a prendre les commandes en ligne.</p>
-    </div>
-    
-</div>
+    </body>
+</html>
