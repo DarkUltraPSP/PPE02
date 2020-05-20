d600806d71ab190174f523a4e72551be1448d83a
Mathilde <mathildebourinet@orange.fr>
2020-05-20 13:10:05 +0200

    modif

--- Developpement/Pages/Contact/Contact.php
+++ Developpement/Pages/Contact/Contact.php
@@ -1,61 +1,55 @@
-<!DOCTYPE html>
-    <head>
-        <link rel="stylesheet" type="text/css" href="css/contact.css" media="all"/>
-    </head>
-    <body>
-    <div class="containerContact">
 
-        <div class="descriptions">
+<div class="containerContact">
 
-            <p class="entete"> Qui sommes-nous ? </p>
-            <p class="texte"> Tacos Of All Time est une entreprise nouvelle avec uniquement des employés qualifiés. De plus, notre Maitre Kebabiste est un chef étoilé de renommée internationale. </p>
+    <div class="descriptions">
 
-        </div>
-
-        <div class="horaires">
-
-            <p class="entete"> Nos Horaires :</p>
-            <table>
-                <tr>
-                    <td class="jour"> Lundi </td>
-                    <td> Ferme </td>
-                </tr>
-                <tr>
-                    <td class="jour"> Mardi </td>
-                    <td> 11h30 - 15h 18h30 - 22h </td>
-                </tr>
-                <tr>
-                    <td class="jour"> Mercredi </td>
-                    <td> 11h30 - 15h 18h30 - 23h</td>
-                </tr>
-                <tr>
-                    <td class="jour"> Jeudi </td>
-                    <td> 11h30 - 15h 18h30 - 23h</td>
-                </tr>
-                <tr>
-                    <td class="jour"> Vendredi </td>
-                    <td> 11h30 - 15h 18h30 - 23h</td>
-                </tr>
-                <tr>
-                    <td class="jour"> Samedi </td>
-                    <td> 11h30 - 15h 18h30 - 23h</td>
-                </tr>
-                <tr>
-                    <td class="jour"> Dimanche </td>
-                    <td> 18h30 - 21h30 </td>
-                </tr>
-            </table>
-
-        </div>
-
-        <div>
-            <p> Nous contacter par téléphone : 04 98 65 45 89 </p>
-        </div>
-
-        <div>
-            <p> A partir du 22 mai nous commencerons a prendre les commandes en ligne.</p>
-        </div>
+        <p class="entete"> Qui sommes-nous ? </p>
+        <p class="texte"> Tacos Of All Time est une entreprise nouvelle avec uniquement des employés qualifiés. De plus, notre Maitre Kebabiste est un chef étoilé de renommée internationale. </p>
 
     </div>
-    </body>
-</html>
+
+    <div class="horaires">
+
+        <p class="entete"> Nos Horaires :</p>
+        <table>
+            <tr>
+                <td class="jour"> Lundi </td>
+                <td> Ferme </td>
+            </tr>
+            <tr>
+                <td class="jour"> Mardi </td>
+                <td> 11h30 - 15h 18h30 - 22h </td>
+            </tr>
+            <tr>
+                <td class="jour"> Mercredi </td>
+                <td> 11h30 - 15h 18h30 - 23h</td>
+            </tr>
+            <tr>
+                <td class="jour"> Jeudi </td>
+                <td> 11h30 - 15h 18h30 - 23h</td>
+            </tr>
+            <tr>
+                <td class="jour"> Vendredi </td>
+                <td> 11h30 - 15h 18h30 - 23h</td>
+            </tr>
+            <tr>
+                <td class="jour"> Samedi </td>
+                <td> 11h30 - 15h 18h30 - 23h</td>
+            </tr>
+            <tr>
+                <td class="jour"> Dimanche </td>
+                <td> 18h30 - 21h30 </td>
+            </tr>
+        </table>
+
+    </div>
+
+    <div>
+        <p> Nous contacter par téléphone : 04 98 65 45 89 </p>
+    </div>
+
+    <div>
+        <p> A partir du 22 mai nous commencerons a prendre les commandes en ligne.</p>
+    </div>
+
+</div>
